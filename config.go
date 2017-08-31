71651c7617470d6a07d2cc930bf09f719620abc7
Stegen Smith <matthsmi@adobe.com>
2017-08-31 17:24:06 +0100

    More improvements to the butler config loader

--- config.go
+++ config.go
@@ -23,18 +23,55 @@ type ButlerConfigClient struct {
 }
 
 type ButlerConfigSettings struct {
-	Handlers map[string]ButlerHandler
+	Managers map[string]ButlerManager
 	Globals  ButlerConfigGlobals
 }
 
 type ButlerConfigGlobals struct {
-	Handlers          []string
-	SchedulerInterval int
-	ExitOnFailure     bool
-	CleanFiles        bool
+	Managers          []string `mapstructure:"config-managers"`
+	SchedulerInterval int      `mapstructure:"scheduler-interval"`
+	ExitOnFailure     bool     `mapstructure:"exit-on-config-failure"`
+	CleanFiles        bool     `mapstructure:"clean-files"`
 }
 
-type ButlerHandler struct {
+type ButlerManager struct {
+	Urls         []string `mapstructure:"urls"`
+	MustacheSubs []string `mapstructure:"mustache-subs"`
+	ManagerOpts  map[string]ButlerManagerOpts
+	Reloader     ButlerManagerReloader
+}
+
+type ButlerManagerOpts struct {
+	Method           string   `mapstructure:"method"`
+	UriPath          string   `mapstructure:"uri-path"`
+	DestPath         string   `mapstructure:"dest-path"`
+	PrimaryConfig    []string `mapstructure:"primary-config"`
+	AdditionalConfig []string `mapstructure:"additional-config"`
+	Opts             map[string]interface{}
+}
+
+type ButlerManagerMethodHttpOpts struct {
+	Retries      int `mapstructure:"retries"`
+	RetryWaitMin int `mapstructure:"retry-wait-min"`
+	RetryWaitMax int `mapstructure:"retry-wait-max"`
+	Timeout      int `mapstructure:"timeout"`
+}
+
+type ButlerManagerReloader struct {
+	Method string `mapstructure:"method"`
+	Opts   interface{}
+}
+
+type ButlerManagerReloaderHttpOpts struct {
+	Host         string `mapstructure:"host"`
+	Port         int    `mapstructure:"port"`
+	Uri          string `mapstructure:"uri"`
+	Method       string `mapstructure:"method"`
+	Payload      string `mapstructure:"payload"`
+	Retries      int    `mapstructure:"retries"`
+	RetryWaitMin int    `mapstructure:"retry-wait-min`
+	RetryWaitMax int    `mapstructure:"retry-wait-max`
+	Timeout      int    `mapstructure:"timeout`
 }
 
 func NewButlerConfigClient(scheme string) (ButlerConfigClient, error) {
@@ -95,10 +132,83 @@ func (c *ButlerConfigClient) Get(val string) (*http.Response, error) {
 	return response, err
 }
 
+func ParseButlerConfigManager(config []byte) (ButlerManager, error) {
+	return ButlerManager{}, nil
+}
+
+func GetButlerManagerOpts(entry string, bc *ButlerConfigSettings) error {
+	var (
+		err         error
+		ManagerOpts ButlerManagerOpts
+	)
+	err = viper.UnmarshalKey(entry, &ManagerOpts)
+	if err != nil {
+		return err
+	}
+
+	switch ManagerOpts.Method {
+	case "http", "https":
+		break
+	default:
+		msg := fmt.Sprintf("unknown manager.method=%v", ManagerOpts.Method)
+		return errors.New(msg)
+	}
+
+	if ManagerOpts.UriPath == "" {
+		return errors.New("no manager.uri-path defined")
+	}
+
+	if ManagerOpts.DestPath == "" {
+		return errors.New("no manager.dest-path defined")
+	}
+
+	if len(ManagerOpts.PrimaryConfig) < 1 {
+		return errors.New("no manager.primary-config defined")
+	}
+
+	// AdditionalConfig is an optional paramater, and it shouldn't matter
+	// if it exists or not ...
+	//if ManagerOpts.AdditionalConfig == ""
+	log.Debugf("GetButlerManagerOpts(): ManagerOpts=%v", ManagerOpts)
+	return nil
+}
+
+func GetButlerConfigManager(entry string, bc *ButlerConfigSettings) error {
+	var (
+		err         error
+		Manager     ButlerManager
+		ManagerOpts ButlerManagerOpts
+	)
+
+	log.Debugf("GetButlerConfigManager(): entered with -> %s and %v", entry, *bc)
+	err = viper.UnmarshalKey(entry, &Manager)
+	if err != nil {
+		return err
+	}
+	if len(Manager.Urls) < 1 {
+		msg := fmt.Sprintf("No urls configured for manager %s", entry)
+		return errors.New(msg)
+	}
+	log.Debugf("GetButlerConfigManager(): Manager.Urls=%v Manager.MustacheSubs=%v", Manager.Urls, Manager.MustacheSubs)
+
+	for _, m := range Manager.Urls {
+		mopts := fmt.Sprintf("%s.%s", entry, m)
+		err = GetButlerManagerOpts(mopts, bc)
+		//err = viper.UnmarshalKey(mopts, &ManagerOpts)
+		if err != nil {
+			return err
+		}
+		log.Debugf("GetButlerConfigManager(): ManagerOpts=%v", ManagerOpts)
+	}
+
+	return nil
+}
+
 func ParseButlerConfig(config []byte) error {
 	var (
 		//handlers []string
-		ButlerConfig ButlerConfigSettings
+		ButlerConfig  ButlerConfigSettings
+		ButlerGlobals ButlerConfigGlobals
 	)
 	// The Butler configuration is in TOML format
 	viper.SetConfigType("toml")
@@ -112,73 +222,60 @@ func ParseButlerConfig(config []byte) error {
 
 	ButlerConfig = ButlerConfigSettings{}
 
-	// Let's grab the exit on failure val
-	if viper.IsSet("globals.exit-on-config-failure") {
-		log.Debugf("ParseButlerConfig(): setting ButlerConfig.Globals.ExitOnFailure to \"%v\"", viper.GetBool("globals.exit-on-config-failure"))
-		ButlerConfig.Globals.ExitOnFailure = viper.GetBool("globals.exit-on-config-failure")
-	} else {
-		ButlerConfig.Globals.ExitOnFailure = false
-		log.Debugf("ParseButlerConfig(): setting ButlerConfig.Globals.ExitOnFailure to \"%v\"", false)
+	// Let's start piecing together the globals
+	err = viper.UnmarshalKey("globals", &ButlerGlobals)
+	if err != nil {
+		log.Fatalf("Unable to decode into struct, %v", err)
 	}
+	log.Debugf("ButlerConfig2=%#v", ButlerGlobals)
+	ButlerConfig.Globals = ButlerGlobals
 
 	// Let's grab some of the global settings
-	if viper.IsSet("globals.scheduler-interval") {
-		ButlerConfig.Globals.SchedulerInterval = viper.GetInt("globals.scheduler-interval")
-	} else {
+	if ButlerConfig.Globals.SchedulerInterval == 0 {
 		ButlerConfig.Globals.SchedulerInterval = ConfigSchedulerInterval
 	}
 
-	if viper.IsSet("globals.clean-files") {
-		ButlerConfig.Globals.CleanFiles = viper.GetBool("globals.config-handlers")
-	} else {
-		ButlerConfig.Globals.CleanFiles = false
-	}
+	log.Debugf("ParseButlerConfig(): globals.config-managers=%#v", ButlerConfig.Globals.Managers)
+	log.Debugf("ParseButlerConfig(): len(globals.config-managers)=%v", len(ButlerConfig.Globals.Managers))
 
-	// We need these handlers. If there are no handlers, then we've really got nothing
-	// to do.
-	if !viper.IsSet("globals.config-handlers") {
+	// If there are no entries for config-managers, then the Unmarshal will create an empty array
+	if len(ButlerConfig.Globals.Managers) < 1 {
 		if ButlerConfig.Globals.ExitOnFailure {
-			log.Fatalf("ParseButlerConfig(): globals.config-handlers unset! exiting...")
+			log.Fatalf("ParseButlerConfig(): globals.config-managers has no entries! exiting...")
 		} else {
-			log.Debugf("ParseButlerConfig(): globals.config-handlers unset!")
-			return errors.New("No globals.config-handlers in butler configuration. Nothing to do.")
-		}
-	} else {
-		ButlerConfig.Globals.Handlers = viper.GetStringSlice("globals.config-handlers")
-	}
-	log.Debugf("ParseButlerConfig(): globals.config-handlers=%#v", ButlerConfig.Globals.Handlers)
-	log.Debugf("ParseButlerConfig(): len(globals.config-handlers)=%v", len(ButlerConfig.Globals.Handlers))
-
-	// If no handlers are set. eg: config-handlers = []
-	if ButlerConfig.Globals.Handlers == nil {
-		if ButlerConfig.Globals.ExitOnFailure {
-			log.Fatalf("ParseButlerConfig(): globals.config-handlers = []! exiting...")
-		} else {
-			log.Debugf("ParseButlerConfig(): globals.config-handlers unset!")
-			return errors.New("globals.config-handlers = []. Nothing to do")
+			log.Debugf("ParseButlerConfig(): globals.config-managers has no entries!")
+			return errors.New("globals.config-managers has no entries. Nothing to do")
 		}
 	}
 
-	// If somehow the length of the config handlers array is < 1
-	// Not sure how to replicate this case, since config-handlers = []
-	// returns nil, but would still like to catch it
-	if len(ButlerConfig.Globals.Handlers) < 1 {
-		if ButlerConfig.Globals.ExitOnFailure {
-			log.Fatalf("ParseButlerConfig(): globals.config-handlers has no entries! exiting...")
-		} else {
-			log.Debugf("ParseButlerConfig(): globals.config-handlers has no entries!")
-			return errors.New("globals.config-handlers has no entries. Nothing to do")
-		}
-	}
-
-	ButlerConfig.Handlers = make(map[string]ButlerHandler)
-	// Now let's start processing the handlers. This is going
-	for _, entry := range ButlerConfig.Globals.Handlers {
+	ButlerConfig.Managers = make(map[string]ButlerManager)
+	// Now let's start processing the managers. This is going
+	for _, entry := range ButlerConfig.Globals.Managers {
 		if !viper.IsSet(entry) {
-			log.Debugf("ParseButlerConfig(): %v is not in the configuration as a handler", entry)
+			if ButlerConfig.Globals.ExitOnFailure {
+				log.Fatalf("ParseButlerConfig(): %v is not in the configuration as a manager! exiting...", entry)
+			} else {
+				log.Debugf("ParseButlerConfig(): %v is not in the configuration as a manager", entry)
+				msg := fmt.Sprintf("Cannot find manager for %s", entry)
+				return errors.New(msg)
+			}
+		} else {
+			log.Debugf("sub=%s", viper.Sub(entry))
+			err = GetButlerConfigManager(entry, &ButlerConfig)
+			if err != nil {
+				if ButlerConfig.Globals.ExitOnFailure {
+					log.Fatalf("ParseButlerConfig(): could not retrieve config options for %v. err=%v", entry, err.Error())
+				} else {
+					log.Debugf("ParseButlerConfig(): could not retrieve config options for %v. err=%v", entry, err.Error())
+					msg := fmt.Sprintf("could not retrieve config options for %v. err=%v", entry, err.Error())
+					return errors.New(msg)
+				}
+			}
+			//ButlerConfig.Managers[entry] = ButlerManager{}
 		}
 	}
 
+	log.Debugf("ButlerConfig.Managers=%#v", ButlerConfig.Managers)
 	return nil
 }
 

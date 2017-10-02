4d3d0ee698f8e08a3ca9eb0d837ce633e82bc6c0
Stegen Smith <matthsmi@adobe.com>
2017-10-02 15:50:43 +0100

    refactored the method and reloader handlers

---
+++ config/reloaders/generic.go
@@ -0,0 +1,34 @@
+package reloaders
+
+import (
+	"errors"
+)
+
+func NewGenericReloader(method string, entry []byte) (Reloader, error) {
+	return GenericReloader{}, errors.New("Generic reloader is not very useful")
+}
+
+type GenericReloader struct {
+	Opts GenericReloaderOpts
+}
+
+type GenericReloaderOpts struct {
+}
+
+func (r GenericReloader) Reload() error {
+	var (
+		res error
+	)
+	return res
+}
+func (r GenericReloader) GetMethod() string {
+	return "none"
+}
+func (r GenericReloader) GetOpts() ReloaderOpts {
+	return r.Opts
+}
+
+func (r GenericReloader) SetOpts(opts ReloaderOpts) bool {
+	r.Opts = opts.(GenericReloaderOpts)
+	return true
+}
---
+++ config/reloaders/reloaders.go
@@ -0,0 +1,47 @@
+package reloaders
+
+import (
+	"encoding/json"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type Reloader interface {
+	Reload() error
+	GetMethod() string
+	GetOpts() ReloaderOpts
+	SetOpts(ReloaderOpts) bool
+}
+
+type ReloaderOpts interface {
+}
+
+func New(entry string) (Reloader, error) {
+	var (
+		err    error
+		result map[string]interface{}
+	)
+
+	key := fmt.Sprintf("%s.reloader", entry)
+
+	err = viper.UnmarshalKey(key, &result)
+	if err != nil {
+		return NewGenericReloader("error", []byte(entry))
+	}
+
+	method := result["method"].(string)
+	jsonRes, err := json.Marshal(result[method])
+	if err != nil {
+		return NewGenericReloader(method, []byte(entry))
+	}
+
+	log.Debugf("reloaders.New() method=%v", method)
+	switch method {
+	case "http", "https":
+		return NewHttpReloader(method, jsonRes)
+	default:
+		return NewGenericReloader(method, jsonRes)
+	}
+}

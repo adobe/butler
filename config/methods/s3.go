58aa330d8d2db797edfff35c4c0d108eeb14f1d7
Stegen Smith <matthsmi@adobe.com>
2017-11-02 23:25:49 +0000

    more tweaks to get s3 to work on butler run

--- config/methods/s3.go
+++ config/methods/s3.go
@@ -1,20 +1,22 @@
 package methods
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	log "github.com/sirupsen/logrus"
+	//"github.com/prometheus/common/log"
 	"github.com/spf13/viper"
 	"net/http"
 	"os"
-	"fmt"
-	"github.com/prometheus/common/log"
 )
 
 type S3Method struct {
 	Bucket     string `json:"bucket-name"`
 	Item       string `json:"file-name"`
+	Manager    string `json::-"`
 	Region     string `json:"region"`
 	Sess       *session.Session
 	Downloader *s3manager.Downloader
@@ -33,8 +35,7 @@ func NewS3Method(manager string, entry string) (Method, error) {
 
 	result.Sess = session.Must(session.NewSession())
 	result.Downloader = s3manager.NewDownloader(result.Sess)
-
-
+	result.Manager = manager
 
 	return result, err
 }
@@ -49,10 +50,10 @@ func (s S3Method) Get(file string) (*http.Response, error) {
 		return nil, err
 	}
 	_, err = s.Downloader.Download(f,
-	&s3.GetObjectInput{
-		Bucket: aws.String(s.Bucket),
-		Key:    aws.String(s.Item),
-	})
+		&s3.GetObjectInput{
+			Bucket: aws.String(s.Bucket),
+			Key:    aws.String(s.Item),
+		})
 	return &http.Response{}, nil
 
 }
